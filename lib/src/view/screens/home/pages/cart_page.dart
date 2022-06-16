db7ad6aef0b5575ec8f456b327409237a7ee38a6
denihero <denihero81@gmail.com>
2022-06-16 15:41:55 +0600

    add cart widget

--- lib/src/view/screens/home/pages/cart_page.dart
+++ lib/src/view/screens/home/pages/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../constants/colors.dart';
 import '../../../constants/styles.dart';
+import 'home/widgets/custom_serch_field.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -9,86 +10,93 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: _size.height,
-      child: Stack(
-        children: [
-          // SingleChildScrollView(
-          //   child: Column(
-          //     children: [
-          // SizedBox(height: 50),
-
-          //       for (int i = 0; i < Product.testProducts.length; i++)
-          //         Padding(
-          //           padding: const EdgeInsets.symmetric(horizontal: 6),
-          //           child: ProductCard(
-          //             product: Product.testProducts[i],
-          //             productCardType: ProductCardType.Cart,
-          //           ),
-          //         ),
-          //     ],
-          //   ),
-          // ),
-          const Center(
-            child: Text("Cart is empty",
-                style: TextStyle(
-                  color: kTextColor,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
-                )),
-          ),
-          Positioned(
-            bottom: 10,
-            child: Container(
-              height: 50,
-              width: MediaQuery.of(context).size.width,
-              color: kPrimaryColor,
-              padding: const EdgeInsets.only(left: 12, right: 4),
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Итого",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      Text(
-                        "0 tmp",
-                        style: kSellingPriceTextStyle,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        elevation: 0,
-                        primary: kLightGreyColor[0],
-                        // primary: kSecondaryColor,
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 12),
-                        child: Text(
-                          "Оформить заказ",
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          actions: <Widget>[Container()],
+          flexibleSpace: Container(
+              height: 30,
+              margin: const EdgeInsets.symmetric(horizontal: 13, vertical: 6),
+              decoration: BoxDecoration(
+                color: kLightGreyColor[0],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const CustomSearchField()
+          )
+      ),
+      body: SizedBox(
+        height: _size.height,
+        child: Stack(
+          children: [
+            ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context,index){
+                  return Container();
+                  //return  CartListTileWidget(product: product);
+                }
+            ),
+            // const Center(
+            //   child: Text("Cart is empty",
+            //       style: TextStyle(
+            //         color: kTextColor,
+            //         fontSize: 24,
+            //         fontWeight: FontWeight.w500,
+            //       )),
+            // ),
+            Positioned(
+              bottom: 10,
+              child: Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                color: kPrimaryColor,
+                padding: const EdgeInsets.only(left: 12, right: 4),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Итого",
                           style: TextStyle(
-                            color: kPrimaryColor,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        Text(
+                          "0 tmp",
+                          style: kSellingPriceTextStyle,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          primary: kLightGreyColor[0],
+                          // primary: kSecondaryColor,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 12),
+                          child: Text(
+                            "Оформить заказ",
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
+                        onPressed: () {},
                       ),
-                      onPressed: () {},
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/src/view/screens/home/pages/home/widgets/product_counter.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ProductCounter extends StatefulWidget {
+  const ProductCounter({Key? key, }) : super(key: key);
+
+
+
+  @override
+  State<ProductCounter> createState() => _ProductCounterState();
+}
+
+class _ProductCounterState extends State<ProductCounter> {
+
+  int counter = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+            splashRadius: 25,
+            padding: const EdgeInsets.only(right: 6,bottom: 5),
+            constraints: const BoxConstraints(),
+            onPressed: () {
+              setState((){
+                if(counter <= 0){
+                  counter = 0;
+                }
+                counter--;
+              });
+            },
+            icon: const Icon(Icons.remove,color: Colors.orange,size: 30,)
+        ),
+        Container(
+          width: 60,
+          height: 30,
+          decoration:  BoxDecoration(
+            border: Border.all(
+              color: Colors.orange
+            ),
+            color: Colors.transparent,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(8),
+            ),
+       ),
+         child: Center(child: Text('$counter',style: const TextStyle(fontSize: 19),)),
+        ),
+        IconButton(
+            splashRadius: 25,
+            padding: const EdgeInsets.only(left: 2,right: 20,bottom: 5),
+            constraints: const BoxConstraints(),
+            onPressed: () {
+              setState((){
+                counter++;
+              });
+            },
+            icon: const Icon(Icons.add,color: Colors.orange,size: 30,)
+        ),
+      ],
+    );
+  }
+}
